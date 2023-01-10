7306732640b537202f8e668e4a8c95c2f5b0942e
Calvin Lapp <lapp.cd@gmail.com>
2023-01-09 19:32:13 -0500

    I lied, added config

--- apps/squid/src/main.rs
+++ apps/squid/src/main.rs
@@ -1,3 +1,6 @@
+use sqlx::{Connection, PgConnection};
+use squid::configuration::get_configuration;
+
 #[macro_use]
 extern crate rocket;
 
@@ -6,7 +9,21 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/hello", routes![index])
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_string = configuration.database.connection_string();
+
+    let _connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let figment = rocket::Config::figment().merge(("port", configuration.application_port));
+
+    let _rocket = rocket::custom(figment)
+        .mount("/", routes![index])
+        .launch()
+        .await?;
+
+    Ok(())
 }
