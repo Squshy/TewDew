22de1255878fb14fb8ef13d5f9b7952bcc4446b0
Calvin Lapp <lapp.cd@gmail.com>
2023-01-12 20:50:26 -0500

    Query accessing DB

--- apps/squid/src/main.rs
+++ apps/squid/src/main.rs
@@ -1,7 +1,8 @@
 use juniper_rocket::{GraphQLRequest, GraphQLResponse};
 use rocket::{routes, State};
+use sqlx::PgPool;
 use squid::configuration::get_configuration;
-use squid::schema::{create_schema, Schema};
+use squid::schema::{create_schema, MyContext, Schema};
 
 #[rocket::get("/")]
 fn graphiql() -> rocket::response::content::RawHtml<String> {
@@ -9,23 +10,38 @@ fn graphiql() -> rocket::response::content::RawHtml<String> {
 }
 
 #[rocket::get("/graphql?<request>")]
-async fn get_graphql_handler(schema: &State<Schema>, request: GraphQLRequest) -> GraphQLResponse {
-    request.execute(&schema, &()).await
+async fn get_graphql_handler(
+    context: &State<MyContext>,
+    request: GraphQLRequest,
+    schema: &State<Schema>,
+) -> GraphQLResponse {
+    request.execute(&schema, &context).await
 }
 
 #[rocket::post("/graphql", data = "<request>")]
-async fn post_graphql_handler(schema: &State<Schema>, request: GraphQLRequest) -> GraphQLResponse {
-    request.execute(&schema, &()).await
+async fn post_graphql_handler(
+    context: &State<MyContext>,
+    request: GraphQLRequest,
+    schema: &State<Schema>,
+) -> GraphQLResponse {
+    request.execute(&schema, &context).await
 }
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let schema = create_schema();
 
     let figment = rocket::Config::figment().merge(("port", configuration.application_port));
+    let context = MyContext {
+        db_pool: connection_pool.clone(),
+    };
 
     let _rocket = rocket::custom(figment)
+        .manage(context)
         .manage(schema)
         .mount(
             "/",
--- apps/squid/src/schema.rs
+++ apps/squid/src/schema.rs
@@ -1,10 +1,17 @@
+use juniper::{Context, GraphQLInputObject, GraphQLObject};
 use juniper::{EmptySubscription, FieldResult, RootNode};
-use juniper::{GraphQLInputObject, GraphQLObject};
+use sqlx::PgPool;
+
+pub struct MyContext {
+    pub db_pool: PgPool,
+}
+
+impl Context for MyContext {}
 
 // The #[graphql(description = "")] seems to be equivalent to doc comments
 // However you can overwrite a comment for GraphQL by using the #[graphql]
 // and the doc comments will still appear in Rust documentation
-#[derive(GraphQLObject)]
+#[derive(sqlx::FromRow, GraphQLObject)]
 #[graphql(description = "Information about a user")]
 struct User {
     #[graphql(description = "The ID of the user")]
@@ -23,19 +30,23 @@ struct NewUser {
 
 pub struct QueryRoot;
 
-#[juniper::graphql_object]
+#[juniper::graphql_object(context = MyContext)]
 impl QueryRoot {
-    fn user(_id: String) -> FieldResult<User> {
-        Ok(User {
-            id: "hehe".to_owned(),
-            username: "coolguy".to_owned(),
-            password: "secret".to_owned(),
-        })
+    async fn user(
+        #[graphql(context)] context: &MyContext,
+        username: String,
+    ) -> FieldResult<Option<User>> {
+        let query_str = format!("SELECT * FROM users WHERE username = '{}'", &username);
+        let user = sqlx::query_as::<_, User>(query_str.as_str())
+            .fetch_optional(&context.db_pool)
+            .await?;
+
+        Ok(user)
     }
 }
 
 pub struct MutationRoot;
-#[juniper::graphql_object]
+#[juniper::graphql_object(context = MyContext)]
 impl MutationRoot {
     fn create_user(new_user: NewUser) -> FieldResult<User> {
         Ok(User {
@@ -46,7 +57,7 @@ impl MutationRoot {
     }
 }
 
-pub type Schema = RootNode<'static, QueryRoot, MutationRoot, EmptySubscription>;
+pub type Schema = RootNode<'static, QueryRoot, MutationRoot, EmptySubscription<MyContext>>;
 
 pub fn create_schema() -> Schema {
     Schema::new(QueryRoot {}, MutationRoot {}, EmptySubscription::new())
