b8a3550bdf6cecc67fad63a318e7ae1a72f18bb7
Calvin Lapp <lapp.cd@gmail.com>
2023-01-14 18:52:51 -0500

    Login with rlly bad returned validaiton but im hungry

--- apps/squid/src/schema/users.rs
+++ apps/squid/src/schema/users.rs
@@ -1,9 +1,9 @@
 use crate::schema::{MutationRoot, MyContext, QueryRoot};
 use argon2::{
-    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
     Argon2,
 };
-use juniper::FieldResult;
+use juniper::{graphql_value, FieldResult};
 use juniper::{GraphQLInputObject, GraphQLObject};
 use uuid::Uuid;
 
@@ -28,20 +28,43 @@ struct NewUser {
     password: String,
 }
 
+enum UserError {
+    NotFound,
+    InvalidUsernameOrPassword,
+}
+
+impl<S: juniper::ScalarValue> juniper::IntoFieldError<S> for UserError {
+    fn into_field_error(self) -> juniper::FieldError<S> {
+        match self {
+            UserError::NotFound => juniper::FieldError::new("User not found", juniper::Value::Null),
+            UserError::InvalidUsernameOrPassword => {
+                juniper::FieldError::new("Invalid username or password", juniper::Value::Null)
+            }
+        }
+    }
+}
+
+impl std::fmt::Display for UserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        // Gotta be better way than duplicating
+        match self {
+            UserError::NotFound => write!(f, "User not found"),
+            UserError::InvalidUsernameOrPassword => write!(f, "Invalid username or password"),
+        }
+    }
+}
+
 #[juniper::graphql_object(context = MyContext)]
 impl QueryRoot {
-    async fn user(
-        #[graphql(context)] context: &MyContext,
-        username: String,
-    ) -> FieldResult<Option<User>> {
+    async fn user(#[graphql(context)] context: &MyContext, username: String) -> FieldResult<User> {
         let user = sqlx::query_as!(
             User,
             r#"SELECT * FROM users WHERE username = $1"#,
             &username
         )
-        .fetch_optional(&context.db_pool)
+        .fetch_one(&context.db_pool)
         .await
-        .unwrap();
+        .map_err(|_e| UserError::NotFound)?;
 
         Ok(user)
     }
@@ -50,14 +73,53 @@ impl QueryRoot {
 // MUTATIONS
 #[juniper::graphql_object(context = MyContext)]
 impl MutationRoot {
-    fn create_user(new_user: NewUser) -> FieldResult<User> {
+    async fn create_user(
+        #[graphql(context)] context: &MyContext,
+        new_user: NewUser,
+    ) -> FieldResult<User> {
         let password_hash = hash_password(&new_user.password)?;
 
+        let user = sqlx::query!(
+            r#"INSERT INTO users (id, username, password) VALUES ($1, $2, $3) RETURNING id"#,
+            &Uuid::new_v4(),
+            &new_user.username,
+            &password_hash
+        )
+        .fetch_one(&context.db_pool)
+        .await?;
+
         Ok(User {
-            id: Uuid::new_v4().to_owned(),
+            id: user.id,
             username: new_user.username,
-            password: password_hash,
+            password: new_user.password,
         })
+    }
+
+    async fn login(
+        #[graphql(context)] context: &MyContext,
+        username: String,
+        password: String,
+    ) -> FieldResult<User> {
+        let user = sqlx::query_as!(User, "SELECT * FROM users WHERE username = $1", &username,)
+            .fetch_one(&context.db_pool)
+            .await?;
+
+        let parsed_hash = PasswordHash::new(&user.password)?;
+
+        let password_bytes: Vec<u8> = password.into_bytes();
+        let is_oki_doki = Argon2::default()
+            .verify_password(&password_bytes, &parsed_hash)
+            .is_ok();
+
+        if !is_oki_doki {
+            return Ok(User {
+                id: Uuid::new_v4(),
+                username: "not_okay".to_string(),
+                password: "not_okay".to_string(),
+            });
+        }
+
+        Ok(user)
     }
 }
 
