0deeb8e290408befe37e1fa7ce69ef88dfc646a8
Calvin Lapp <lapp.cd@gmail.com>
2023-01-17 19:31:03 -0500

    Login

--- apps/squid/src/schema/users.rs
+++ apps/squid/src/schema/users.rs
@@ -57,6 +57,7 @@ impl std::fmt::Display for UserError {
 #[juniper::graphql_object(context = MyContext)]
 impl QueryRoot {
     async fn user(#[graphql(context)] context: &MyContext, username: String) -> FieldResult<User> {
+        // TODO: Create a lil model thingy to handle all db access
         let user = sqlx::query_as!(
             User,
             r#"SELECT * FROM users WHERE username = $1"#,
@@ -102,25 +103,31 @@ impl MutationRoot {
     ) -> FieldResult<User> {
         let user = sqlx::query_as!(User, "SELECT * FROM users WHERE username = $1", &username,)
             .fetch_one(&context.db_pool)
-            .await?;
+            .await
+            .map_err(|_e| UserError::InvalidUsernameOrPassword)?;
 
-        let parsed_hash = PasswordHash::new(&user.password)?;
-
-        let password_bytes: Vec<u8> = password.into_bytes();
-        let is_oki_doki = Argon2::default()
-            .verify_password(&password_bytes, &parsed_hash)
-            .is_ok();
-
-        if !is_oki_doki {
-            return Ok(User {
-                id: Uuid::new_v4(),
-                username: "not_okay".to_string(),
-                password: "not_okay".to_string(),
-            });
-        }
+        verify_password(&password, &user.password)?;
 
         Ok(user)
     }
+}
+
+fn verify_password(password: &String, user_password: &String) -> Result<Option<bool>, UserError> {
+    let parsed_hash = match PasswordHash::new(&user_password) {
+        Ok(password_hash) => password_hash,
+        Err(_e) => return Err(UserError::InvalidUsernameOrPassword),
+    };
+
+    let password_bytes: Vec<u8> = password.clone().into_bytes();
+    let is_oki_doki = Argon2::default()
+        .verify_password(&password_bytes, &parsed_hash)
+        .is_ok();
+
+    if !is_oki_doki {
+        return Err(UserError::InvalidUsernameOrPassword);
+    }
+
+    Ok(None)
 }
 
 fn hash_password(password: &String) -> Result<String, argon2::password_hash::Error> {
