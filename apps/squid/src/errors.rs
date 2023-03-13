1957f849ddc99a02f9d81a5a469c96d694fda3ff
Calvin Lapp <lapp.cd@gmail.com>
2023-03-13 19:19:22 -0400

    Remove actix error for servererror

--- apps/squid/src/errors.rs
+++ apps/squid/src/errors.rs
@@ -1,5 +1,4 @@
 // See: https://github.com/clifinger/canduma/blob/master/src/errors.rs
-use actix_web::HttpResponse;
 use serde::Serialize;
 use thiserror::Error;
 
@@ -21,19 +20,6 @@ pub enum ServiceError {
 impl From<sqlx::Error> for ServiceError {
     fn from(_value: sqlx::Error) -> Self {
         ServiceError::InternalDatabaseError
-    }
-}
-
-impl actix_web::ResponseError for ServiceError {
-    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
-        match self {
-            ServiceError::Unauthorized => HttpResponse::Unauthorized().finish(),
-            ServiceError::InternalServerError => HttpResponse::InternalServerError().finish(),
-            ServiceError::InternalDatabaseError => {
-                HttpResponse::InternalServerError().body("Internal Database Error")
-            }
-            ServiceError::BadRequest(s) => HttpResponse::BadRequest().body(s.to_string()),
-        }
     }
 }
 
