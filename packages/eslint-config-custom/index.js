6bae097fd2f2f6c0af5e8c25dd7bf116fcc2aae0
Calvin Lapp <lapp.cd@gmail.com>
2023-03-18 13:55:37 -0400

    idk hope it works. eslint config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,14 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-  },
+    extends: [
+        'turbo',
+        'prettier',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {},
 };
