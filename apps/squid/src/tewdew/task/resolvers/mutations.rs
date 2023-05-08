ae1abfcbcc2747a3e8822ad283244cac503a4996
Calvin Lapp <lapp.cd@gmail.com>
2023-05-08 19:26:15 -0400

    Added fielderrors to task creation

--- apps/squid/src/tewdew/task/resolvers/mutations.rs
+++ apps/squid/src/tewdew/task/resolvers/mutations.rs
@@ -2,6 +2,7 @@ use crate::errors::ServiceResult;
 use crate::jwt::models::Claims;
 use crate::schema::lib::{get_claims_from_context, get_pool_from_context};
 use crate::schema::middleware::Middleware;
+use crate::schema::models::FieldError;
 use crate::tewdew::task::models::{NewTask, NewTaskError, Task, UpdateTaskError, UpdatedTask};
 use crate::tewdew::task::services::{create, delete, update};
 use async_graphql::{Context, Object};
@@ -10,10 +11,10 @@ use uuid::Uuid;
 #[derive(Default)]
 pub struct TaskMutation;
 
-#[derive(async_graphql::Union)]
-pub enum CreateTaskResult {
-    Ok(Task),
-    Err(NewTaskError),
+#[derive(async_graphql::SimpleObject)]
+pub struct CreateTaskResult {
+    task: Option<Task>,
+    task_errors: Option<Vec<FieldError>>,
 }
 
 #[derive(async_graphql::Union)]
@@ -34,14 +35,22 @@ impl TaskMutation {
     ) -> ServiceResult<CreateTaskResult> {
         let new_task = match NewTask::validate(tewdew_id, title, completed) {
             Ok(val) => val,
-            Err(e) => return Ok(CreateTaskResult::Err(e)),
+            Err(e) => {
+                return Ok(CreateTaskResult {
+                    task: None,
+                    task_errors: Some(e),
+                })
+            }
         };
 
         let pool = get_pool_from_context(ctx)?;
         let Claims { sub, .. } = get_claims_from_context(ctx)?;
         let task = create(pool, &new_task, &sub).await?;
 
-        Ok(CreateTaskResult::Ok(task))
+        Ok(CreateTaskResult {
+            task: Some(task),
+            task_errors: None,
+        })
     }
 
     #[graphql(guard = "Middleware::Authorized")]
