01160408b6383d28f3fa8c5a6d6dae13366ff7de
Calvin Lapp <lapp.cd@gmail.com>
2023-06-02 20:03:56 -0400

    Removed notifications from landing page

--- apps/canoe/src/LandingPage.tsx
+++ apps/canoe/src/LandingPage.tsx
@@ -1,34 +1,9 @@
-import { useRef } from 'react';
-import useNotification from './contexts/Notifications';
-
 export default function LandingPage() {
-    const { showSuccess, showError, showWarning } = useNotification();
-    const ref = useRef<HTMLInputElement>(null);
-
     return (
-        <div className="w-full h-full bg-red-50 flex space-x-2">
-            <input type="text" ref={ref} />
-            <div>Hey!</div>
-            <button
-                onClick={() =>
-                    showSuccess({
-                        message: ref.current?.value || 'nice!',
-                        expiresInMs: Infinity,
-                    })
-                }
-            >
-                Success
-            </button>
-            <button
-                onClick={() =>
-                    showError({ message: 'nice!', title: `${Math.random()}` })
-                }
-            >
-                Error
-            </button>
-            <button onClick={() => showWarning({ message: 'nice!' })}>
-                Warning
-            </button>
+        <div className="w-full h-full flex">
+            <div>
+                <p className="text-xl">Welcome</p>
+            </div>
         </div>
     );
 }
