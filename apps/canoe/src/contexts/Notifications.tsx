da3b0a29b83a61af3c813d29dc634857f81c9605
Calvin Lapp <lapp.cd@gmail.com>
2023-06-13 20:20:11 -0400

    Modified notification context

--- apps/canoe/src/contexts/Notifications.tsx
+++ apps/canoe/src/contexts/Notifications.tsx
@@ -12,19 +12,32 @@ import React, {
 const DEFAULT_MESSAGE_EXPIRY_TIME_MS = 3500; // 3.5 seconds
 const ANIMATION_DURATION_MS = 200;
 
+type NodeCbFn = ((node: NotificationNode) => void) | undefined;
 type NotificationFnParams = Partial<
-    Pick<NotificationNode, 'title' | 'expiresInMs'>
+    Pick<NotificationNode, 'title' | 'expiresInMs' | 'onNotify' | 'onExpire'>
 > & {
     message: string;
 };
 
 type NotificationFn = (args: NotificationFnParams) => NotificationNode;
+type UpdateNodeParams = Partial<
+    Pick<
+        NotificationNode,
+        'expiresInMs' | 'onExpire' | 'isDuplicate' | 'onDuplicated'
+    >
+>;
 
-type ExpireNodeFn = (key: string) => void;
+type ExpireNodeFn = (node: NotificationNode) => void;
+type UpdateNodeFn = (
+    node: NotificationNode,
+    params: UpdateNodeParams
+) => NotificationNode;
+
 type InternalNotificationContext = {
     getNodeMap: () => Map<string, NotificationNode>;
     subscribe: (cb: () => void) => () => boolean;
     expireNode: ExpireNodeFn;
+    updateNode: UpdateNodeFn;
 };
 
 type NotificationContext = {
@@ -35,6 +48,7 @@ type NotificationContext = {
     notifyError: NotificationFn;
     notifyWarning: NotificationFn;
     expireNode: ExpireNodeFn;
+    updateNode: UpdateNodeFn;
 };
 
 type NotificationType = typeof NotificationType[keyof typeof NotificationType];
@@ -45,13 +59,17 @@ const NotificationType = {
     WARNING: 'warning',
 } as const;
 
-type NotificationNode = {
+export type NotificationNode = {
     key: string;
-    message: string;
-    title?: string | undefined;
     type: NotificationType;
+    title?: string | undefined;
+    message: string;
     createdAt: number;
-    expiresInMs: number;
+    expiresInMs: number | null;
+    onNotify?: NodeCbFn;
+    onExpire?: NodeCbFn;
+    onDuplicated?: NodeCbFn;
+    isDuplicate: boolean;
 };
 
 const InternalContext = createContext<InternalNotificationContext | null>(null);
@@ -66,11 +84,15 @@ function createNotificationNode({
     message,
     title,
     expiresInMs = DEFAULT_MESSAGE_EXPIRY_TIME_MS,
+    onExpire,
+    onNotify,
 }: {
     type: NotificationType;
     message: string;
     title?: string | undefined;
-    expiresInMs?: number;
+    expiresInMs?: number | null;
+    onExpire?: NodeCbFn;
+    onNotify?: NodeCbFn;
 }): NotificationNode {
     const messageKey = message.replace(/\s/g, '').toLocaleLowerCase();
     const titleKey = title?.replace(/\s/g, '').toLocaleLowerCase() || '';
@@ -85,13 +107,17 @@ function createNotificationNode({
         message,
         expiresInMs,
         title,
+        onNotify,
+        onExpire,
         createdAt: Date.now(),
+        isDuplicate: false,
     };
 }
 
 type NotificationProps = {
     node: NotificationNode;
     expireNode: ExpireNodeFn;
+    updateNode: UpdateNodeFn;
 };
 
 function NotificationIcon({ node }: { node: NotificationNode }) {
@@ -142,51 +168,54 @@ function NotificationIcon({ node }: { node: NotificationNode }) {
     );
 }
 
-function Notification({ node, expireNode }: NotificationProps) {
+function Notification({ node, expireNode, updateNode }: NotificationProps) {
     const [scale, setScale] = useState(false);
-    const timeoutsRef = useRef<NodeJS.Timeout[]>([]);
-
-    const expireAfterAnimating = useMemo(
-        () =>
-            function () {
-                // Animate
-                setScale(false);
-                const expireTimeout = setTimeout(() => {
-                    expireNode(node.key);
-                }, ANIMATION_DURATION_MS);
-
-                timeoutsRef.current.push(expireTimeout);
-            },
-        [expireNode, node]
-    );
+    const animationTimeoutRef = useRef<NodeJS.Timeout>();
 
     useEffect(() => {
         setScale(true);
 
         // Allow us to set infinite messages until a user clears it manually
-        if (node.expiresInMs === Infinity) {
+        if (!node.expiresInMs || node.expiresInMs === Infinity) {
             return;
         }
 
         const timeout = setTimeout(() => {
-            expireAfterAnimating();
-        }, node.expiresInMs);
+            setScale(false);
+        }, Math.max(node.expiresInMs - 500, 500));
 
-        timeoutsRef.current.push(timeout);
-        const timeouts = timeoutsRef.current;
-
-        return () => {
-            for (const timeout in timeouts) {
+        updateNode(node, {
+            onDuplicated: () => {
                 clearTimeout(timeout);
-            }
+            },
+        });
+
+        const animationTimeout = animationTimeoutRef.current;
+        return () => {
+            clearTimeout(timeout);
+            clearTimeout(animationTimeout);
         };
-    }, [node.expiresInMs, expireAfterAnimating]);
+    }, [node, updateNode]);
+
+    function expireAfterAnimating() {
+        setScale(false);
+        const timeout = setTimeout(() => {
+            expireNode(node);
+        }, ANIMATION_DURATION_MS);
+        animationTimeoutRef.current = timeout;
+    }
+
+    function hehe() {
+        updateNode(node, { isDuplicate: false });
+    }
 
     return (
         <div
-            className={`transition-all ease-out duration-[${ANIMATION_DURATION_MS}] rounded-md p-2 m-2 bg-slate-100 border-2 border-slate-200 max-w-2 ${
-                scale ? 'scale-100' : 'scale-0'
-            }`}
+            className={`transition-all ease-out duration-[${ANIMATION_DURATION_MS}] rounded-md p-2 m-2 bg-slate-100
+             border-2 border-slate-200 max-w-2 ${
+                 scale ? 'scale-100' : 'scale-0'
+             } ${node.isDuplicate ? 'animate-wiggle' : ''}`}
+            onMouseOver={hehe}
         >
             <div className="w-64 flex flex-col space-y-2">
                 <div className="w-full flex space-x-2 items-start">
@@ -233,48 +262,128 @@ function Notification({ node, expireNode }: NotificationProps) {
 // Used the fast context to have the notifications render only within their own lil happy space
 // @see https://github.com/jherr/fast-react-context/blob/main/fast-context-generic/src/createFastContext.tsx
 export function NotificationProvider(props: NotificationProviderProps) {
+    const timeoutsRef = useRef<Map<string, NodeJS.Timeout>>(new Map());
     const store = useRef(new Map<string, NotificationNode>());
     const subscribers = useRef(new Set<() => void>());
 
-    const getNodeMap = useCallback(() => store.current, []);
+    useEffect(() => {
+        const timeouts = timeoutsRef.current;
 
-    const addNode = useCallback((node: NotificationNode) => {
-        // Do not notify subscribers or update to new map reference
-        // if we currently have a similar node.
-        if (store.current.has(node.key)) {
-            return node;
-        }
-
-        store.current.set(node.key, node);
-        store.current = new Map(store.current);
-        for (const cb of subscribers.current) {
-            cb();
-        }
-
-        return node;
+        return () => {
+            for (const timeout of timeouts.values()) {
+                clearTimeout(timeout);
+            }
+        };
     }, []);
 
-    const expireNode = useCallback((key: string) => {
-        const deleted = store.current.delete(key);
+    const getNodeMap = useCallback(() => store.current, []);
+
+    const expireNode = useCallback((node: NotificationNode) => {
+        const deleted = store.current.delete(node.key);
 
         if (!deleted) {
             return;
         }
 
+        if (node.onExpire) {
+            node.onExpire(node);
+        }
+
+        timeoutsRef.current.delete(node.key);
         store.current = new Map(store.current);
         for (const cb of subscribers.current) {
             cb();
         }
     }, []);
+
+    const addNode = useCallback(
+        (node: NotificationNode) => {
+            if (store.current.get(node.key)) {
+                node.isDuplicate = true;
+
+                if (node.onDuplicated) {
+                    node.onDuplicated(node);
+                }
+
+                const timeout = timeoutsRef.current.get(node.key);
+                if (timeout) {
+                    timeoutsRef.current.delete(node.key);
+                    clearTimeout(timeout);
+                }
+            }
+            store.current.set(node.key, node);
+            store.current = new Map(store.current);
+
+            // Ignores `null` and `0`
+            if (node.expiresInMs && node.expiresInMs !== Infinity) {
+                const timeout = setTimeout(() => {
+                    expireNode(node);
+                }, node.expiresInMs);
+
+                timeoutsRef.current.set(node.key, timeout);
+            }
+
+            for (const cb of subscribers.current) {
+                cb();
+            }
+
+            return node;
+        },
+        [expireNode]
+    );
+
+    const updateNode = useCallback(
+        (node: NotificationNode, params: UpdateNodeParams) => {
+            if (params.expiresInMs !== undefined) {
+                const nodeTimeout = timeoutsRef.current.get(node.key);
+                if (nodeTimeout) {
+                    timeoutsRef.current.delete(node.key);
+                    clearTimeout(nodeTimeout);
+                }
+
+                // Ignores `null` and `0`
+                if (params.expiresInMs && params.expiresInMs !== Infinity) {
+                    const timeout = setTimeout(() => {
+                        expireNode(node);
+                    }, params.expiresInMs);
+
+                    timeoutsRef.current.set(node.key, timeout);
+                }
+            }
+
+            if (params.onExpire) {
+                node.onExpire = params.onExpire;
+            }
+
+            if (params.onDuplicated) {
+                node.onDuplicated = params.onDuplicated;
+            }
+
+            if (params.isDuplicate !== undefined) {
+                node.isDuplicate = params.isDuplicate;
+            }
+
+            store.current.set(node.key, node);
+            store.current = new Map(store.current);
+
+            for (const cb of subscribers.current) {
+                cb();
+            }
+
+            return node;
+        },
+        [expireNode]
+    );
 
     const subscribe = useCallback((cb: () => void) => {
         subscribers.current.add(cb);
         return () => subscribers.current.delete(cb);
     }, []);
 
-    // TODO: Add generic dull notification type
     const notify = useCallback(
-        (params: NotificationFnParams & { type: NotificationType }) => {
+        (
+            params: NotificationFnParams & { type: NotificationType }
+        ): NotificationNode => {
             const node = createNotificationNode(params);
             return addNode(node);
         },
@@ -310,10 +419,11 @@ export function NotificationProvider(props: NotificationProviderProps) {
                 notifyWarning,
                 notifyError,
                 expireNode,
+                updateNode,
             }}
         >
             <InternalContext.Provider
-                value={{ getNodeMap, subscribe, expireNode }}
+                value={{ getNodeMap, subscribe, expireNode, updateNode }}
             >
                 <Hehe />
             </InternalContext.Provider>
@@ -323,24 +433,26 @@ export function NotificationProvider(props: NotificationProviderProps) {
 }
 
 function Hehe() {
-    const { state, expireNode } = useInternalNotification();
+    const { state, expireNode, updateNode } = useInternalNotification();
+
+    const nodes = useMemo(() => {
+        return Array.from(state.values()).sort((a, b) => {
+            const bExpire = b.expiresInMs ?? Infinity;
+            const aExpire = b.expiresInMs ?? Infinity;
+            return b.createdAt + bExpire - (a.createdAt + aExpire);
+        });
+    }, [state]);
 
     return (
         <div className="absolute right-0 z-50">
-            {Array.from(state.values())
-                .sort(
-                    (a, b) =>
-                        b.createdAt +
-                        b.expiresInMs -
-                        (a.createdAt + a.expiresInMs)
-                )
-                .map((node) => (
-                    <Notification
-                        key={node.key}
-                        node={node}
-                        expireNode={expireNode}
-                    />
-                ))}
+            {nodes.map((node) => (
+                <Notification
+                    key={node.key}
+                    node={node}
+                    expireNode={expireNode}
+                    updateNode={updateNode}
+                />
+            ))}
         </div>
     );
 }
@@ -354,7 +466,11 @@ function useInternalNotification() {
 
     const state = useSyncExternalStore(store.subscribe, store.getNodeMap);
 
-    return { state, expireNode: store.expireNode };
+    return {
+        state,
+        expireNode: store.expireNode,
+        updateNode: store.updateNode,
+    };
 }
 
 export default function useNotification() {
