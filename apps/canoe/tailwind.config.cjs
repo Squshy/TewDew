02826ebf075e35175f43566d4572ba88939351f4
Calvin Lapp <lapp.cd@gmail.com>
2023-06-13 20:20:25 -0400

    Added animate-wiggle to tailwind config

--- apps/canoe/tailwind.config.cjs
+++ apps/canoe/tailwind.config.cjs
@@ -1,4 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.tsx', './index.html'],
+    theme: {
+        extend: {
+            keyframes: {
+                wiggle: {
+                    '0%, 100%': { transform: 'rotate(-0.5deg)' },
+                    '50%': { transform: 'rotate(0.5deg)' },
+                },
+            },
+            animation: {
+                wiggle: 'wiggle 200ms ease-in-out infinite',
+            },
+        },
+    },
 };
