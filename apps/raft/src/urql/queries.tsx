a62edb0207a41104f581ff282603a46e4406fdd6
Calvin Lapp <lapp.cd@gmail.com>
2023-07-25 20:13:34 -0400

    Added list tewdew query

--- apps/raft/src/urql/queries.tsx
+++ apps/raft/src/urql/queries.tsx
@@ -1,12 +1,31 @@
-import { useQuery, UseQueryArgs } from 'urql';
+import { AnyVariables, useQuery, UseQueryArgs } from 'urql';
 //
-import { MeDocument, MeQuery, MeQueryVariables } from 'tewgql';
+import {
+    ListTewDewsDocument,
+    ListTewDewsQuery,
+    ListTewDewsQueryVariables,
+    MeDocument,
+    MeQuery,
+    MeQueryVariables,
+} from 'tewgql';
 
-export function useMeQuery(
-    options: Omit<UseQueryArgs<MeQueryVariables>, 'query' | 'variables'> = {}
-) {
+type QueryVars<T extends AnyVariables> = Omit<
+    UseQueryArgs<T>,
+    'query' | 'variables'
+>;
+
+export function useMeQuery(options: QueryVars<MeQueryVariables> = {}) {
     return useQuery<MeQuery, MeQueryVariables>({
         query: MeDocument,
+        ...options,
+    });
+}
+
+export function useListTewDewsQuery(
+    options: QueryVars<ListTewDewsQueryVariables> = {}
+) {
+    return useQuery<ListTewDewsQuery, ListTewDewsQueryVariables>({
+        query: ListTewDewsDocument,
         ...options,
     });
 }
